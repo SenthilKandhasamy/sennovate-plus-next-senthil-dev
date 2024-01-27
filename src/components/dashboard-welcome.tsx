96d6fe2f7c8d906018a9da97a8b4af228327c3d8
Shobhan Biswas <61372335+shobhanbiswas11@users.noreply.github.com>
2024-01-27 18:36:18 +0530

    minor changes

--- src/components/dashboard-welcome.tsx
+++ src/components/dashboard-welcome.tsx
@@ -4,8 +4,6 @@ import { getUserType } from "@/user-type";
 import { useSession } from "next-auth/react";
 import AdminDashboard from "./dashboards/admin";
 import EmployeeDashboard from "./dashboards/employee";
-import ReferralDashboard from "./dashboards/referral";
-import ResellerDashboard from "./dashboards/reseller";
 
 export default function DashboardWelcome() {
   const { status, data } = useSession();
@@ -19,10 +17,6 @@ export default function DashboardWelcome() {
       return <AdminDashboard />;
     case "employee":
       return <EmployeeDashboard />;
-    case "referral":
-      return <ReferralDashboard />;
-    case "reseller":
-      return <ResellerDashboard />;
 
     default:
       return <div>Welcome to the Portal</div>;
