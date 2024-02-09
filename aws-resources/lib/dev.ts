8d60ba3803d1cede8a15ebcff569fbb82ee98d44
Shobhan Biswas <61372335+shobhanbiswas11@users.noreply.github.com>
2024-02-09 19:46:37 +0530

    added json for backend resources config

--- aws-resources/lib/dev.ts
+++ aws-resources/lib/dev.ts
@@ -1,11 +1,19 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { dev } from "../env";
+import { readFileSync } from "fs";
+import { resolve } from "path";
 import { SennovatePlusUserPool } from "./user-pool";
+
+const env = JSON.parse(
+  readFileSync(resolve(__dirname, "../env.json"), "utf-8")
+);
 
 export class SennovatePlusDevStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new SennovatePlusUserPool(this, "SennovatePlusDevUserPool", dev.userPool);
+    new SennovatePlusUserPool(this, "SennovatePlusDevUserPool", {
+      ...env.dev.userPool,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
--- aws-resources/lib/prod.ts
+++ aws-resources/lib/prod.ts
@@ -1,12 +1,19 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { prod } from "../env";
+import { readFileSync } from "fs";
+import { resolve } from "path";
 import { SennovatePlusUserPool } from "./user-pool";
+
+const env = JSON.parse(
+  readFileSync(resolve(__dirname, "../env.json"), "utf-8")
+);
 
 export class SennovatePlusProdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    new SennovatePlusUserPool(this, "SennovatePlusProdUserPool", prod.userPool);
+    new SennovatePlusUserPool(this, "SennovatePlusProdUserPool", {
+      ...env.prod.userPool,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
   }
 }
