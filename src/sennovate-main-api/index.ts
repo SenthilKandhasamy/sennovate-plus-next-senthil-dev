a4fe1adb247c97c41ddc989c9da83286d90e1c07
Ramkumar Jayaraman <80389936+rjayaraman23@users.noreply.github.com>
2024-03-22 18:13:48 +0530

    Update index.ts

--- src/sennovate-main-api/index.ts
+++ src/sennovate-main-api/index.ts
@@ -1,5 +1,5 @@
 import { Service } from "./service.type";
-
+import { Servicefalse } from "./service.type";
 export async function getService({ slug }: { slug?: string } = {}) {
   let url = `${process.env.SENNOVATE_MAIN_WEBSITE_URL}/wp-json/sennovate/v1/services`;
   if (slug) {
@@ -17,4 +17,14 @@ export async function getService({ slug }: { slug?: string } = {}) {
     },
   });
   return res.json() as Promise<Service[]>;
+}
+export async function getServicefalse() {
+  let url = `${process.env.SENNOVATE_MAIN_WEBSITE_URL}/wp-json/sennovate/v1/servicesfalse?slug=common-document`;
+  const res = await fetch(url, {
+    cache: "no-store",
+    headers: {
+      Cookie: "wordpress_=hack-no-cache",
+    },
+  });
+  return res.json() as Promise<Servicefalse[]>;
 }
