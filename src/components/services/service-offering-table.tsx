f005ac2334306fa326e43b0f7fe2bee987a0e83b
SenthilKandhasamy <161854148+SenthilKandhasamy@users.noreply.github.com>
2024-04-29 19:39:19 +0530

    Update service-offering-table.tsx

--- src/components/services/service-offering-table.tsx
+++ src/components/services/service-offering-table.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { ServiceOffering1, ServiceOffering2, ServicePoints1 } from "@/sennovate-main-api/service.type";
-
+import { ServiceOffering1, ServiceOffering2 } from "@/sennovate-main-api/service.type";
 import {
   Table,
   TableBody,
@@ -13,19 +12,19 @@ import {
 } from "@nextui-org/react";
 
 interface Props {
-  offering: ServiceOffering1 | ServiceOffering2; // Allow either ServiceOffering1 or ServiceOffering2
+  offering: ServiceOffering1;
 }
 
-export default function ServiceOfferingTable({ offering }: Props) {
-  function renderPoint(point: any) {
-    return {
-      key: point.title,
-      offering: point.title,
-      essential: <div className="text-center px-2">{point.essential}</div>,
-      advance: <div className="text-center px-2">{point.advance}</div>,
-    };
-  }
+function renderPoint(point: any) {
+  return {
+    key: point.title,
+    offering: point.title,
+    essential: <div className="text-center px-2">{point.essential}</div>,
+    advance: <div className="text-center px-2">{point.advance}</div>,
+  };
+}
 
+export function ServiceOfferingTable1({ offering }: Props) {
   const columns = [
     {
       key: "offering",
@@ -54,7 +53,7 @@ export default function ServiceOfferingTable({ offering }: Props) {
       offering: <span className="text-primary-500">{c.heading}</span>,
     });
     if (c.has_points) {
-      c.points?.forEach((p: ServicePoints1) => { // Assuming ServicePoints1 is the correct type
+      c.points?.forEach((p) => {
         a.push(renderPoint(p));
       });
     }
