9ea7fe933cb70bc17bbaa5acf8ad841e60e0ed77
SenthilKandhasamy <161854148+SenthilKandhasamy@users.noreply.github.com>
2024-04-29 14:42:37 +0530

    Update service.type.ts

--- src/sennovate-main-api/service.type.ts
+++ src/sennovate-main-api/service.type.ts
@@ -17,9 +17,7 @@ export type ServicePoints = {
   essential?: string;
   advance?: string;
   on-prem_essential?: string;
-  on-prem_advance?: string; 
-  cloud_essential?: string;
-  cloud_advance?: string;
+
 };
 
 export type ServiceOffering = {
